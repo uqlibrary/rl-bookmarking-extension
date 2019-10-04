004dbc285b6c9faa6db30d6c012f007b172b0184
Richard Gubby <rg@talis.com>
2019-10-04 19:23:38 +0100

    Make this work in chrome AND firefox

--- js/background.js
+++ js/background.js
@@ -3,28 +3,21 @@
  *
  * This only does anything when the bookmarking button is clicked
  */
-var executedScript = false;
 chromeOrBrowser().browserAction.onClicked.addListener(function(currentTab) {
     // If no institution has been set, go to the options page to set one
     getActiveTenant(function (tenantCode) {
         if (!tenantCode) {
-            if (window.confirm(chromeOrBrowser().i18n.getMessage('noTenantAlert'))) {
-                // browser.runtime.openOptionsPage() not supported by ms-edge, so here's a workaround
-                chromeOrBrowser().tabs.create({
-                    url: chromeOrBrowser().extension.getURL("options.html")
-                  });
-            }
-        }
-        if (!executedScript) {
+            // browser.runtime.openOptionsPage() not supported by ms-edge, so here's a workaround
+            chromeOrBrowser().tabs.create({
+                url: chromeOrBrowser().extension.getURL("options.html")
+            });
+        } else {
             chromeOrBrowser().tabs.executeScript(null, {
                 file: "/js/bookmarker.js",
                 runAt: 'document_end'
             }, function () {
-                executedScript = true;
                 chromeOrBrowser().tabs.sendMessage(currentTab.id, {tenantCode: tenantCode});
             });
-        } else {
-            chromeOrBrowser().tabs.sendMessage(currentTab.id, {tenantCode: tenantCode});
         }
     });
 });
