c7620601921db83b696154772b3e2a891d6cb3e8
Richard Gubby <rg@talis.com>
2019-10-04 17:43:13 +0100

    Revert changes back

--- js/bookmarker.js
+++ js/bookmarker.js
@@ -1,33 +1,36 @@
 /*
  * Content script to inject the dynamic bookmarking JS directly into the page.
  */
-
-function chromeOrBrowser() {
-  return this.browser || chrome;
-}
-/**
- * Injected into page to redirect to the dynamic page parser
- *
- * @param {String} tenantCode
- */
-function bookmarkPage(tenantCode) {
-  if (document.getElementById('bookmarkingScript')) {
-    document.getElementById('bookmarkingScript').remove();
+(function() {
+  function chromeOrBrowser() {
+    return this.browser || chrome;
   }
 
-  var bookmarker = document.createElement('script');
-  bookmarker.setAttribute(
-    'src',
-    'https://bookmarking.talis.com/' + tenantCode + '/parser?bookmarkButtonVersion=1&uri=' +
-      encodeURIComponent(encodeURI(window.location.href)) + '&bookmarkVersion=1&title=' +
-      encodeURIComponent(document.title) + '&hasJquery=no'
+  /**
+   * Injected into page to redirect to the dynamic page parser
+   *
+   * @param {String} tenantCode
+   */
+  function bookmarkPage(tenantCode) {
+    if (document.getElementById('bookmarkingScript')) {
+      document.getElementById('bookmarkingScript').remove();
+    }
+
+    var bookmarker = document.createElement('script');
+    bookmarker.setAttribute(
+        'src',
+        'https://bookmarking.talis.com/' + tenantCode + '/parser?bookmarkButtonVersion=1&uri=' +
+        encodeURIComponent(encodeURI(window.location.href)) + '&bookmarkVersion=1&title=' +
+        encodeURIComponent(document.title) + '&hasJquery=no'
     );
-  bookmarker.setAttribute('id', 'bookmarkingScript');
+    bookmarker.setAttribute('id', 'bookmarkingScript');
 
-  if (document.body) {
-    document.body.appendChild(bookmarker);
+    if (document.body) {
+      document.body.appendChild(bookmarker);
+    }
   }
-}
-chromeOrBrowser().runtime.onMessage.addListener(function(message) {
-  bookmarkPage(message.tenantCode);
-});
+
+  chromeOrBrowser().runtime.onMessage.addListener(function (message) {
+    bookmarkPage(message.tenantCode);
+  });
+})();
