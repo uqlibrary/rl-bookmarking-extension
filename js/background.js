06142b199cf3b6fbd49057b33a5bdb9b717118ac
Mike Rudd <mike.rudd@talis.com>
2022-09-30 13:56:24 +0100

    ASP-2684: refactor for scripting differences per v3 spec (https://developer.chrome.com/docs/extensions/mv3/mv3-migration-checklist/#api-tabs)

--- js/background.js
+++ js/background.js
@@ -1,11 +1,20 @@
 import {chromeOrBrowser, getActiveTenant} from "./tenants";
 
+async function getCurrentTab() {
+    let queryOptions = {active: true, lastFocusedWindow: true};
+    // `tab` will either be a `tabs.Tab` instance or `undefined`.
+    let [tab] = await chrome.tabs.query(queryOptions);
+    return tab;
+}
+
+let tab = await getCurrentTab();
+
 /**
  * Background script.
  *
  * This only does anything when the bookmarking button is clicked
  */
-chromeOrBrowser().browserAction.onClicked.addListener(function(currentTab) {
+chromeOrBrowser().browserAction.onClicked.addListener(function (currentTab) {
     // If no institution has been set, go to the options page to set one
     getActiveTenant(function (tenant) {
         if (!tenant) {
@@ -14,12 +23,13 @@ chromeOrBrowser().browserAction.onClicked.addListener(function(currentTab) {
                 url: chromeOrBrowser().extension.getURL("options.html")
             });
         } else {
-            chromeOrBrowser().tabs.executeScript(null, {
-                file: "/js/bookmarker.js",
-                runAt: 'document_end'
-            }, function () {
-                chromeOrBrowser().tabs.sendMessage(currentTab.id, {tenant: tenant});
-            });
+            chromeOrBrowser().scripting.executeScript(
+              {
+                  target: {tabId: tab.id},
+                  files: ['/js/bookmarker.js']
+              }, function () {
+                  chromeOrBrowser().tabs.sendMessage(currentTab.id, {tenant: tenant});
+              });
         }
     });
 });
